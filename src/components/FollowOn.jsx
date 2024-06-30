937e11d985e5c0c3b75bc6bb35316e23b8615647
Rishhh <grishi349@gmail.com>
2024-06-30 14:25:10 +0530

    nuilding the whole Single GIF showing gifPage with a functional Sidebar

--- src/components/FollowOn.jsx
+++ src/components/FollowOn.jsx
@@ -4,6 +4,7 @@ import {
   FaLinkedin,
   FaXTwitter,
   FaYoutube,
+  FaArrowDown,
 } from "react-icons/fa6";
 
 const FollowOn = () => {
@@ -11,15 +12,24 @@ const FollowOn = () => {
     <div
       className="faded-text pt-2" //custom - faded-text
     >
-      <span>Follow Me Here:</span>
+      <div className="flex gap-2 items-center faded-text">
+        <span>Follow Me Here</span>
+        <FaArrowDown />
+      </div>
       <div className="flex gap-4 pt-3">
-        <a href="https://www.linkedin.com/in/rishi-raj-gupta45/">
+        <a
+          href="https://www.linkedin.com/in/rishi-raj-gupta45/"
+          target="_blank"
+        >
           <FaLinkedin size={20} />
         </a>
-        <a href="https://github.com/rishiigupta04">
+        <a href="https://github.com/rishiigupta04" target="_blank">
           <FaGithub size={20} />
         </a>
-        <a href="https://x.com/rishiigupta04?t=UgmSdTUvplYuCE9MtelQDg&s=09">
+        <a
+          href="https://x.com/rishiigupta04?t=UgmSdTUvplYuCE9MtelQDg&s=09"
+          target="_blank"
+        >
           <FaXTwitter size={20} />
         </a>
       </div>
--- src/pages/single-gif.jsx
+++ src/pages/single-gif.jsx
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/gif-context";
+import Gif from "../components/Gif";
+import { HiMiniChevronDown, HiMiniChevronUp } from "react-icons/hi2";
+import FollowOn from "../components/FollowOn";
+import { HiOutlineExternalLink } from "react-icons/hi";
 
+const contentType = ["gifs", "stickers", "text"];
 const GifPage = () => {
-  return <div>GifPage</div>;
+  const [gif, setGif] = useState({});
+  const [relatedGifs, setRelatedGifs] = useState([]);
+  const [readMore, setReadMore] = useState(false);
+
+  const { type, slug } = useParams();
+
+  const { gifFetcher } = GifState();
+
+  const fetchGif = async () => {
+    const gifId = slug.split("-");
+    const { data } = await gifFetcher.gif(gifId[gifId.length - 1]);
+    const { data: related } = await gifFetcher.related(
+      gifId[gifId.length - 1],
+      {
+        type: "gifs",
+        limit: 10,
+      }
+    );
+    setGif(data);
+    setRelatedGifs(related);
+  };
+
+  useEffect(() => {
+    // if (!contentType.includes(type)) {
+    //   throw new Error("Invalid Content Type");
+    // }
+
+    fetchGif();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-4 my-10 gap-4">
+      <div className="hidden sm:block ">
+        {gif?.user && (
+          <>
+            <div className="flex gap-1">
+              <img
+                src={gif?.user?.avatar_url}
+                alt={gif?.user?.display_name}
+                className="h-14 rounded-full"
+              />
+
+              <div className="px-2">
+                <div className="font-bold">{gif?.user?.display_name}</div>
+                <div className="faded-text">@{gif?.user?.username}</div>
+              </div>
+            </div>
+
+            {gif?.user?.description && (
+              <p className="py-4 whitespace-pre-line faded-text">
+                {readMore
+                  ? gif?.user?.description
+                  : gif?.user?.description.slice(0, 100) + ".."}
+                {gif?.user?.description.length > 100 && (
+                  <div
+                    className="flex items-center faded-text cursor-pointer"
+                    onClick={() => setReadMore(!readMore)}
+                  >
+                    {readMore ? (
+                      <>
+                        Read less <HiMiniChevronUp size={20} />
+                      </>
+                    ) : (
+                      <>
+                        Read more <HiMiniChevronDown size={20} />
+                      </>
+                    )}
+                  </div>
+                )}
+              </p>
+            )}
+          </>
+        )}
+
+        <FollowOn />
+
+        <div className="divider" />
+        {gif?.source && (
+          <div>
+            <span className="font-extrabold text-gray-400">Source</span>
+            <div className="flex gap-2 items-center text-sm font-bold">
+              <HiOutlineExternalLink size={25} />
+              <a
+                href={gif?.source}
+                target="_blank"
+                className="faded-text truncate"
+              >
+                {gif?.source}
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="col-span-4 sm:col-span-3">
+        <div className="flex gap-6">
+          <div className="w-full sm:w-3/4">
+            <div className="text-sm sm:text-xl truncate mb-2 faded-text">
+              {gif.title}
+            </div>
+            <Gif gif={gif} hover={false} />
+
+            {/* mobile UI */}
+          </div>
+          favorites/share/embed
+        </div>
+
+        <div>
+          <span className="font-extrabold">Related GIFs</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GifPage;
